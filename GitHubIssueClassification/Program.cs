1bd45db673d8b3fdf1183508d621998bd66cde0d
e <ejtwitty@gmail.com>
2019-04-24 02:01:07 -0400

    Added paths to downloaded files and global variables

--- GitHubIssueClassification/Program.cs
+++ GitHubIssueClassification/Program.cs
@@ -10,6 +10,16 @@ namespace GitHubIssueClassification
 {
     class Program
     {
+        private static string _appPath => Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]);
+        private static string _trainDataPath => Path.Combine(_appPath, "..", "..", "..", "Data", "issues_train.tsv");
+        private static string _testDataPath => Path.Combine(_appPath, "..", "..", "..", "Data", "issues_test.tsv");
+        private static string _modelPath => Path.Combine(_appPath, "..", "..", "..", "Models", "model.zip");
+
+        private static MLContext _mlContext;
+        private static PredictionEngine<GitHubIssue, IssuePrediction> _predEngine;
+        private static ITransformer _trainedModel;
+        static IDataView _trainingDataView;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
